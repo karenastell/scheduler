7aeb0d5383721d026a0b4a52cb667d4712e23c85
Karen Astell <KarenAstell@gmail.com>
2020-04-28 18:08:37 -0400

    psuedo code complete

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+// when the page opens, the date and time are displayed at the top
+// there are text areas where the user can type evens for the day
+//      there is a save button
+//      there is a section with the time
+//      there is a section with the text
+
+
+// ********************* Saving events in the scheduler *******************
+// when the save button is clicked the event is stored in local storage
+//      when the page is refreshed the events will still be where they were inputted
+
+
+
+// *********************** Elapsed Time ***********************************
+// depending on the current time the text areas will change colors
+//       past = grey
+//       present = green
+//       future = blue
