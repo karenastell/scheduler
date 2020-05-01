752a417cb0b20b151d84e7f9d9a8c4bb0c069b13
Karen Astell <KarenAstell@gmail.com>
2020-05-01 16:30:19 -0400

    saves event in local storage in seperate keys for each hour

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,51 +23,69 @@ setInterval(function () {
 // ********************* Saving events in the scheduler *******************
 // when the save button is clicked the event is stored in local storage
 //      when the page is refreshed the events will still be where they were inputted
-var nine = $(".9").val();
-var ten = $(".10").val();
-var eleven = $(".11").val();
-var twelve = $(".12").val();
-var thirteen = $(".13").val();
-var fourteen = $(".14").val();
-var fifteen = $(".15").val();
-var sixteen = $(".16").val();
-var seventeen = $(".17").val();
-var eighteen = $(".18").val();
-var ninteen = $(".19").val();
+
+// function saveEvents() {
+//   var saveButtonEl = $(this);
+//   var inputEl = document.getElementsByClassName(".hour");
+//   //console.log(inputEl.val());
+//   var storageKey = saveButtonEl.attr("data-hour");
+//   localStorage.setItem(storageKey, inputEl.val());
+// }
+
+// var nine = $(".9").val();
+// var ten = $(".10").val();
+// var eleven = $(".11").val();
+// var twelve = $(".12").val();
+// var thirteen = $(".13").val();
+// var fourteen = $(".14").val();
+// var fifteen = $(".15").val();
+// var sixteen = $(".16").val();
+// var seventeen = $(".17").val();
+// var eighteen = $(".18").val();
+// var ninteen = $(".19").val();
+
+// function saveEvents() {
+//   var eventsObject = {
+//     nine: nine,
+//     ten: ten,
+//     eleven: eleven,
+//     twelve: twelve,
+//     thirteen: thirteen,
+//     fourteen: fourteen,
+//     fifteen: fifteen,
+//     sixteen: sixteen,
+//     seventeen: seventeen,
+//     eighteen: eighteen,
+//     ninteen: ninteen,
+//   };
+
+//   console.log(eventsObject);
+//   var event = getEventsFromLS();
+
+//   event.push(eventsObject);
+//   var eventsJSON = JSON.stringify(event);
+//   localStorage.setItem("event", eventsJSON);
+// }
+
+// function getEventsFromLS() {
+//   event = localStorage.getItem("event");
+//   if (event) {
+//     return JSON.parse(event);
+//   } else {
+//     return [];
+//   }
+// }
 
 function saveEvents() {
-  var eventsObject = {
-    nine: nine,
-    ten: ten,
-    eleven: eleven,
-    twelve: twelve,
-    thirteen: thirteen,
-    fourteen: fourteen,
-    fifteen: fifteen,
-    sixteen: sixteen,
-    seventeen: seventeen,
-    eighteen: eighteen,
-    ninteen: ninteen,
-  };
-
-  console.log(eventsObject);
-  var event = getEventsFromLS();
-  event.push(eventsObject);
-  var eventsJSON = JSON.stringify(event);
-  localStorage.setItem("event", eventsJSON);
-  //console.log("eventsObject.nine", eventsObject.nine);
+  var saveBtnEl = $(this);
+  var storageKey = saveBtnEl.attr("data-time");
+  //var inputEl = $(".hour").val();
+  var inputEl = saveBtnEl.parent().siblings(".hour");
+  console.log(inputEl.val());
+  localStorage.setItem(storageKey, inputEl.val());
 }
 
-saveEvents();
-
-function getEventsFromLS() {
-  event = localStorage.getItem("event");
-  if (event) {
-    return JSON.parse(event);
-  } else {
-    return [];
-  }
-}
+$(".btn").on("click", saveEvents);
 
 // another data-save for the buttons?
 
